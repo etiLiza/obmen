c0f694765338aa572dba5d2a7375406fda636f5d
etiLiza <stolyarova_ea@mail.ru>
2025-07-31 18:30:18 +0700

    созданa функция exchange, для получения курса валют

--- obmen.py
+++ obmen.py
@@ -4,7 +4,26 @@ import pprint
 from tkinter import*
 from tkinter import messagebox as mb
 
+from bottle import response
 
+
+def exchange():
+    code = entry.get()#получает информацию из поля entry
+
+    if code:# если код есть то работаем дальше, если код не введен то программа должна попросить ввести код
+        try:
+            response = requests.get("open.er-api.com/v6/latest/USD")# получаем информацию в переменную
+            response.raise_for_status()# проверяем статус ,если получаем ответ 200 то все хорошо
+            data = response.json()#переводим json в обычный питон словарь
+            if code in data["rates"]# если в "rates" существует введенный код . то мы берем это значение
+                exchange_rate = data["rates"][code]#в пременную положим словарь, и из него выбираем значение по ключу
+                mb.showinfo("курс обмена", f"Курс:{exchange_rate}{code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка!", f"Валюта {code} не найдена!")
+        except Exception as e:
+            mb.showerror ("Ошибка!", f"произошла ошибка:{e}.")
+    else:
+        mb.showwarning("Внимание!", "Введите код валюты")
 
 
 window = Tk()
